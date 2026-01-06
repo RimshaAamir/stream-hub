ff13ad0231c38f2c7906912c632cb73e11bbfe80
Rimsha Aamir <rimshaamir03@gmail.com>
2026-01-06 12:24:32 +0500

    feat: implement user model with password hashing and JWT authentication.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,10 @@
 import mongoose, {Mongoose, Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
 const UserSchema = new Schema(
     {
-        username: {
+        userName: {
             type: String,
             required: true,
             unique: true,
@@ -17,7 +19,7 @@ const UserSchema = new Schema(
             lowercase: true, 
             trim: true,
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
@@ -50,5 +52,46 @@ const UserSchema = new Schema(
         timestamps: true
     }
 )
+
+UserSchema.pre("save", async function(next){    // can't use callback here as we need this reference
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);    // salts and hash rounds 
+    next();
+})
+
+//custom method injection
+UserSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password) // compare returns true/false
+}
+
+
+UserSchema.methods.generateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            fullName: this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+UserSchema.methods.generateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", UserSchema)
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Mongoose, Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema (
     {
@@ -39,5 +40,13 @@ const videoSchema = new Schema (
         timestamps: true
     }
 )
+
+
+
+//mongoose-aggregate-paginate-v2 is a Mongoose plugin that adds pagination support 
+// to aggregation pipelines, returning only the requested page of results along 
+// with useful pagination metadata (total pages, next page, previous page, 
+// total documents, etc.).
+videoSchema.plugin(mongooseAggregatePaginate); 
 
 export const Video = mongoose.model("Video", videoSchema)
